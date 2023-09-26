b97a76d5f191180aeea4e18c9c5a5f974af7439a
shivam sharma <shivam.sh9557@gmail.com>
2023-09-26 12:32:13 +0530

    added word count utility by file

--- backend/server.py
+++ backend/server.py
@@ -28,7 +28,7 @@ output_file_name = 'output' # without extension
 
 @cross_origin()
 @app.route('/word_count', methods = ['GET', 'POST'])
-def home():
+def word_count_text():
     if(request.method == 'GET'):
         try:
             input_data = request.args.get('input_data')
@@ -70,6 +70,65 @@ def home():
         except:
             return jsonify({'status': "error", 'data': "Something went wrong"})
   
+
+@cross_origin()
+@app.route('/word_count_file', methods = ['POST'])
+def word_count_file():
+    if(request.method == 'POST'):
+        # try:
+            uploaded_file = request.files.get('uplodedFile')
+            selected_language = request.form.get('language')
+            
+            print("Selected language: " , selected_language)
+            print("Uploaded File: " , uploaded_file)
+            file_type= uploaded_file.filename.split('.')[-1]
+
+            if(file_type == 'txt'):
+              pass
+            else:
+              return jsonify({'status': "error", 'data': "Invalid file format. Allowed files with extension [ txt ]"})
+            
+            uploaded_file.save("./uploded_file/" + uploaded_file.filename)
+            
+            # get data from text file
+            with open("./uploded_file/" + uploaded_file.filename, encoding="utf-8") as f:
+              input_data = f.read()
+            print("data from text file : ", input_data)
+            
+            ## get word count from input data
+            # split data with white space
+            filtered_input_data = filter_data(input_data, selected_language)
+            list_word = filtered_input_data.split()
+            print(len(list_word))
+
+            # create dictionary of words with count
+            
+            email_regex = re.compile(r'^[\w]+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$')
+            counts = dict()
+            for word in list_word:
+              ## ignore numeric values, email and URL
+              if not word.isdigit():
+                if re.fullmatch(email_regex, word) is None:
+                  if word in counts:
+                      counts[word] += 1
+                  else:
+                      counts[word] = 1
+            
+            ## conert into excel          
+            KeysList = list(counts.keys())
+            ValuesList = list(counts.values())
+            df = pd.DataFrame({'List of Words': KeysList, 'words Count': ValuesList,})
+            
+            # df.to_csv('file.csv', index=False)
+            df.to_excel('../output_files/'+ output_file_name +'.xlsx', index=False)
+
+    
+            return jsonify({'status': "success", 'data': "Done"})
+        # except PermissionError:
+        #   return jsonify({'status': "error", 'data': "Output file is open. First close it and try again"})
+        # except:
+        #     return jsonify({'status': "error", 'data': "Something went wrong"})
+  
 # driver function
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True, port=5001)
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-
+// this function is for input text 
 function submitSelected() {
   $('#res_error').text("");
   $('#download_file').html("")
@@ -15,7 +15,7 @@ function submitSelected() {
     $('#res_success').text("");
   }else{
     console.log("input data : ", input_data, ' languageSelected', languageSelected);
-    const url = "http://localhost:5000";
+    const url = "http://localhost:5001";
 
     $.ajax({
       type: "GET",
@@ -33,7 +33,63 @@ function submitSelected() {
       }
     }).catch(err => {
       console.log("print Error");
-        $('#res_error').text("Something wrong happened");
+      $('#res_success').text("");
+      $('#res_error').text("Something wrong happened");
+    })
+  }
+
+  
+}
+
+// This function is for file input 
+function submitFile() {
+  $('#res_error').text("");
+  $('#download_file').html("")
+  $('#res_success').text("Please wait while file is being processed ...");
+
+  let languageSelected = $("#floatingSelect").val()
+
+  // check value is not empty
+  var uploadedFile = $("#UploadFile")[0].files;
+  console.log("length of file: " + uploadedFile.length)
+  console.log("Uploaded File: " + uploadedFile)
+
+  if(uploadedFile.length === 0) {
+    $('#res_error').text("Please select file");
+    $('#res_success').text("");
+  }else{
+    console.log("input file : ", uploadedFile[0], ' languageSelected', languageSelected);
+
+    const url = "http://localhost:5001";
+    var form_data = new FormData()
+    form_data.append('language', languageSelected)
+    form_data.append('uplodedFile', uploadedFile[0])
+
+    $.ajax({
+      type: "POST",
+      url: url + "/word_count_file",
+      contentType: false,
+      cache: false,
+      processData: false,
+      // dataType: "json",
+      // contentType: 'application/json;charset=UTF-8',
+      // data: JSON.stringify({data: "hello"})
+      data: form_data
+    }).done(function (res) {
+      console.log("res ", res);
+      if (res.status == "success") {
+        console.log("print success");
+        $('#res_success').text("Data Saved");
+        $('#download_file').html("<a href='./output_files/output.xlsx' class='btn btn-danger' download>Download File</a>");
+      } else {
+        console.log("print Error");
+        $('#res_error').text(res.data);
+        $('#res_success').text("");
+      }
+    }).catch(err => {
+      console.log("print Error ", err);
+      $('#res_success').text("");
+      $('#res_error').text("Something wrong happened");
     })
   }
 
